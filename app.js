f5e2e769bfa340ab265d6d7dc081b32b118c19c6
Joel <joelfilho02812@gmail.com>
2022-11-11 10:38:39 -0300

    add funcionalidades carrinho: quantidades e total

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const vm = new Vue({
     
     data: {
         produtos: [],
-        produto: false
+        produto: false,
+        carrinho: [],
     },
 
     filters: {
         numeroPreco(valor) {
             return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+        }
+    },
+
+    computed: {
+        carrinhoTotal() {
+            
+            let total = 0;
+
+            if (this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    total += item.preco;
+                })
+            }
+
+            return total;
         }
     },
 
@@ -42,6 +58,18 @@ const vm = new Vue({
             if (target === currentTarget) {
                 this.produto = false;
             }
+        },
+
+        adicionarItem() {
+            this.produto.estoque--;
+
+            const {id, nome, preco} = this.produto;
+
+            this.carrinho.push({id, nome, preco})
+        },
+
+        removerItem(index) {
+            this.carrinho.splice(index, 1);
         }
     },
 
